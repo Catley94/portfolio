e89f1eb7c758672d590bcc581ac76ddb593e9b15
Samuel Catley <scp94373@gmail.com>
2019-03-02 23:26:05 +0000

    Fadein Animations
    Added FadeIn Animations for top card

--- src/Info/Info.js
+++ src/Info/Info.js
@@ -10,10 +10,10 @@ const Info = (props) => {
     }
     return (
         <div className="topSection" >
-            <h1 className="whiteText">{props.name}</h1>
-            <h2 className="whiteText">{props.job}</h2>
-            <p className="description">{props.desc}</p>
-            <a href={props.github} className="whiteText">My Github</a> 
+            <h1 className="testText1">{props.name}</h1>
+            <h2 className="testText4">{props.job}</h2>
+            <p className="testText8">{props.desc}</p>
+            <a href={props.github} className="testText12">My Github</a> 
            
         </div>
     )
