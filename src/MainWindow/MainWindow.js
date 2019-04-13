8a7b285f07cfe962c9d0dbd6f38d1ff6f4d63c42
Samuel Catley <scp94373@gmail.com>
2019-04-13 15:36:47 +0100

    Fix styling issues and organised
    Changed Projects to sit side by side and fixed mobile version styling issues.

--- src/MainWindow/MainWindow.js
+++ src/MainWindow/MainWindow.js
@@ -2,26 +2,12 @@ import React from 'react';
 import './main.css';
 import Projects from './Projects/Projects';
 import SideProjects from './Projects/SideProjects/SideProjects';
-import Info from '../Info/Info';
+
+import Footer from '../Footer/Footer';
 
 
 const MainWindow = (props) => {
-    const projects = (
-        <div>
-          {props.projects.map(project => {
-            return (
-            <Projects 
-                id={project.id}
-                projectN={project.ProjectName}
-                projectDesc={project.ProjectDescription}
-                projectLink={project.ProjectLink}
-                githubLink={project.GithubLink}
-            />      )
-                }
-            )
-          }
-        </div>
-      );
+    
       const sideProjects = (
         <div>
           {props.sideProjects.map(project => {
@@ -40,36 +26,37 @@ const MainWindow = (props) => {
     return(
         
         <div className="Main">
-          <div className="InfoComponent">
-            <Info 
-              name={props.name} 
-              job={props.job}
-              github={props.github}
-              desc={props.desc}
-            />
-          </div>
+        
             <div className="midCard">
+              <div className="information">
                 <h2 classNames="titles">Technologies I have used</h2>
                 <div className="technologies">
                   <h3>HTML CSS JAVASCRIPT SASS REACT SOLIDITY</h3>
-                  
                 </div>
                 <h2 className="titles">React Projects</h2>
-                <div>
-                    {projects}
-                </div>
-                <h2 className="titles">React and Solidity Projects</h2>
-                <div>
-                    {sideProjects}
-                </div>
+              </div>
+              <div className="Project0">
+                <Projects 
+                  projectN={props.projects[0].ProjectName}
+                  projectDesc={props.projects[0].ProjectDescription}
+                  projectLink={props.projects[0].ProjectLink}
+                  githubLink={props.projects[0].GithubLink}
+                />
+              </div>
+              <div className="Project1">
+                <Projects 
+                  projectN={props.projects[1].ProjectName}
+                  projectDesc={props.projects[1].ProjectDescription}
+                  projectLink={props.projects[1].ProjectLink}
+                  githubLink={props.projects[1].GithubLink}
+                />
+              </div>
+              <h2 className="titleSideProjects">React and Solidity Projects</h2>
+              <div className="sideProjects">
+                  {sideProjects}
+              </div>
             </div>
-            <div className="bottomCard">
-            <p className="whiteText">Please use this email to contact me <span />
-                <a href="mailto:s.catley@live.co.uk" className="whiteText">s.catley@live.co.uk</a>
-                    <br />
-                <em>This website will evolve with my skills.</em>
-            </p>
-            </div>
+            
         </div>
     )
 }
