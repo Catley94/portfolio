0e19ca512b166b95f993a5368e0b54725d18ba0c
Samuel Catley <scp94373@gmail.com>
2020-02-09 14:43:10 +0000

    Updated exploresr website link
    Was linking to heroku, now linking to new domain name.

--- src/App.js
+++ src/App.js
@@ -64,7 +64,7 @@ class App extends Component {
           id: 1,
           WebsiteName: 'Goudhurst Explorers',
           WebsiteDescription: 'My partner and I currently are Explorer Leaders, and we\'re trying to increase our awareness as Explorers in general, as Explorers are quite new to the scouting industry, to be an Explorer, you must be between the age of 14-18. I also created this website not only for awareness but for the schedule of what the coming weeks.',
-          WebsiteLink: 'https://goudhurst-explorers.herokuapp.com/',
+          WebsiteLink: 'https://www.goudhurstexplorers.com',
           WebsiteImg: goudhurstImg
 
         }
